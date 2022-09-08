617d853f6d0044f467b313d36ba76573ef9287ea
Rostyslavderii <105818935+Rostyslavderii@users.noreply.github.com>
2022-09-08 21:02:53 +0300

    done

---
+++ src/js/task-09.js
@@ -0,0 +1,19 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+<<<<<<< HEAD
+
+
+// Напиши скрипт, который изменяет цвета фона элемента <body> 
+// через инлайн стиль при клике на button.
+// change-color и выводит значение цвета в span.color.
+
+document.querySelector(".change-color").addEventListener('click', colorChange);
+
+function colorChange() {
+  const bgColor = getRandomHexColor();
+  document.body.style.backgroundColor = bgColor;
+  document.querySelector('span.color').textContent = bgColor;
+}
+=======
+>>>>>>> parent of 22c97b4 (Done)
